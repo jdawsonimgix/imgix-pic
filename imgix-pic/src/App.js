53bfcaec5af356bedf7ecfa1a09f19e4c469751e
Jamie Dawson <Jamie@imgixs-mbp.attlocal.net>
2021-09-27 17:30:23 -0700

    Created all borders for image example

--- imgix-pic/src/App.js
+++ imgix-pic/src/App.js
@@ -7,26 +7,67 @@ export default function App() {
   const [borderSelected, borderFunction] = useState(
     buildURL("https://jdawsons3.imgix.net/vapor-cat-1.jpeg", {
       q:  PIC_DATA.q,
-      border: PIC_DATA.border
+      bg: PIC_DATA.bg,
+      border: PIC_DATA.border,
+      pad: PIC_DATA.pad
     })
   );
 
+  /*
+    "q": "65", 
+    "bg": "C9E5ED",
+    "border": "10,000000",
+    "pad": "35",
+  */
+
   const updateBorder = (word) => {
-    if (word === "bold") {
-      //borderFunction(borderSelected + "border=20,000000");
+    if (word === "classic") {
+      borderFunction(
+      buildURL("https://jdawsons3.imgix.net/vapor-cat-1.jpeg", {
+        q:  PIC_DATA.q,
+        bg: "C9E5ED",
+        border: "10,000000",
+        pad: "35"
+      })
+      )
     }
+
+    if (word === "bold") {
+      borderFunction(
+      buildURL("https://jdawsons3.imgix.net/vapor-cat-1.jpeg", {
+        q:  PIC_DATA.q,
+        bg: "C9E5ED",
+        border: "10,000000",
+        pad: "0"
+      })
+      )
+    }
+
+    if (word === "ever") {
+      borderFunction(
+      buildURL("https://jdawsons3.imgix.net/vapor-cat-1.jpeg", {
+        q:  PIC_DATA.q,
+        bg: "C9E5ED",
+        border: "10,FFFFFF",
+        pad: "65"
+      })
+      )
+    }
+
+
   };
 
   return (
     <div className="App">
-      {PIC_DATA.q}
       <h4>
         Here is an example on how imgix can be used for creating borders and
         cropping
       </h4>
 
       <Imgix src={borderSelected} width={300} height={300} />
+      <button onClick={() => updateBorder("classic")}>Classic</button>
       <button onClick={() => updateBorder("bold")}>Bold</button>
+      <button onClick={() => updateBorder("ever")}>Ever</button>
     </div>
   );
 }
--- imgix-pic/src/pic_data.js
+++ imgix-pic/src/pic_data.js
@@ -1,6 +1,9 @@
 export let PIC_DATA = {
-    "q": "60", 
-    "border": "0,000000"
+    "q": "65", 
+    "bg": "C9E5ED",
+    "border": "10,000000",
+    "pad": "35",
+    
 }
 
 export default PIC_DATA;
